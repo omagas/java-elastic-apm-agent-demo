9d27718c20f57829d026024332d263d88dbb9bdb
Louie Zheng <omaga1130@gmail.com>
2025-12-17 14:28:45 +0800

    ADD delay log

--- mysql-service/src/main/java/com/example/mysql_service/MainController.java
+++ mysql-service/src/main/java/com/example/mysql_service/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import co.elastic.apm.api.CaptureSpan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 @RequestMapping(path = "/friend")
@@ -15,21 +17,27 @@ public class MainController {
     @Autowired
     private FriendRepository friendRepository;
 
+    Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @CaptureSpan(value = "delay")
     public void delay(int length) {
+        logger.info("going to sleep for " + length + "ms.");
         try {
             Thread.sleep(length);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        logger.info("finished sleep");
     }
 
     @PostMapping(path = "/add")
     public @ResponseBody String addNewFriend(@RequestParam String name) {
+        logger.info("adding new friend: " + name + ".");
         delay(1000);
         Friend friend = new Friend();
         friend.setName(name);
         friendRepository.save(friend);
+        logger.info("added: " + name);
         return "Saved";
     }
 
