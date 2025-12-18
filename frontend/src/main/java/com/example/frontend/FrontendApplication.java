4f38440c6446b1ac8b4d2834efc5454bbfe99420
Louie Zheng <omaga1130@gmail.com>
2025-12-18 11:10:17 +0800

    feat: Add new Spring Boot frontend service with Thymeleaf for displaying friends, and update gitignore.

--- frontend/src/main/java/com/example/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/example/frontend/FrontendApplication.java
@@ -2,21 +2,21 @@ package com.example.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class FrontendApplication {
-    private static String getFriendList() {
-        RestTemplate rest = new RestTemplate();
-        return rest.getForObject("http://localhost:8080/friend/all", String.class);
-    }
 
     @GetMapping("/friend-list")
-    public String friendList() {
-        return getFriendList();
+    public String friendList(Model model) {
+        RestTemplate rest = new RestTemplate();
+        Friend[] friends = rest.getForObject("http://localhost:8080/friend/all", Friend[].class);
+        model.addAttribute("friends", friends);
+        return "friends";
     }
 
     public static void main(String[] args) {
